92f9ea94ebd7e7d8c658e56d50ba2added657d00
moroz <moroz@axinom.com>
2019-02-04 17:35:43 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,58 @@
+using System;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+
+namespace ZipArchiveApp
+{
+    static class Program
+    {
+        static void UnZip()
+        {
+            string zipFilePath;
+            double zipFileSize;
+            string extractFolderPath;
+            double initialDirectorySize;
+            double directorySizeWithUnzippedFiles;
+            double unzippedFilesSize;
+            double ratioInPercents;
+
+
+            Console.WriteLine("Input the path to a zip archive to be extracted including its file name and extention and press [Enter] button:");
+            zipFilePath = Console.ReadLine();
+            Console.WriteLine("Input the path to the folder where the zip archive will be extracted and press [Enter] button:");
+            extractFolderPath = Console.ReadLine();
+
+            try
+            {
+                DirectoryInfo info = new DirectoryInfo(extractFolderPath);
+                initialDirectorySize = info.EnumerateFiles().Sum(file => file.Length);
+
+                zipFileSize = new FileInfo(zipFilePath).Length;
+
+                ZipFile.ExtractToDirectory(zipFilePath, extractFolderPath);
+                Console.WriteLine($"Zip archive successfully extracted to the \"{extractFolderPath}\" folder");
+
+                directorySizeWithUnzippedFiles = info.EnumerateFiles().Sum(file => file.Length);
+
+                unzippedFilesSize = directorySizeWithUnzippedFiles - initialDirectorySize;
+                ratioInPercents = (1 - zipFileSize / unzippedFilesSize) * 100;
+
+                Console.WriteLine($"Compression ratio of your archive was {ratioInPercents} %");
+            }
+            catch
+            {
+                Console.WriteLine("Something went wrong. Try again.");
+                UnZip();
+            }
+
+        }
+
+        static void Main()
+        {
+            UnZip();
+            Console.WriteLine("Press any key to close the screen...");
+            Console.ReadKey();
+        }
+    }
+}
