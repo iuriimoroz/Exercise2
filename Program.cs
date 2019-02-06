a948dfc9bd5d99293f9d360bc9ca7e71202630c6
moroz <moroz@axinom.com>
2019-02-06 17:33:19 +0200

    Fixed some bugs

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ namespace ZipArchiveApp
 {
     static class Program
     {
-        static public void UnZip()
+        static void UnZip()
         {
             string zipFilePath;
             double zipFileSize;
@@ -15,14 +15,14 @@ namespace ZipArchiveApp
             double directorySize;
             double ratioInPercents;
             string zipFileName;
-            string oldestFile = "";
+            string oldestFile = ""; // The variable assigned during its declaration, because it will be never assigned when zip archive does not contain files and compiler error appears in that case
 
             Console.WriteLine("Input the path to a zip archive to be extracted including its file name and extention and press [Enter] button:");
             zipFilePath = Console.ReadLine();
             Console.WriteLine("Input the path to the folder where the zip archive will be extracted and press [Enter] button:");
             extractFolderPath = Console.ReadLine();
 
-            // User input will be manipulated below - in order to avoid exeption by the wrong input try-catch block used
+            // User input will be manipulated below - in order to avoid exceptions by the wrong input try-catch block used
             try
             {
                 // Zip file name is needed to create a folder with the same name as the name of the zip archive to be extracted
@@ -52,9 +52,9 @@ namespace ZipArchiveApp
                 ZipFile.ExtractToDirectory(zipFilePath, extractFolderPath);
                 Console.WriteLine($"Zip archive successfully extracted to the \"{extractFolderPath}\" folder");
 
-                // Next two lines calculate the size (in bytes) of the folder with extracted files
-                DirectoryInfo info = new DirectoryInfo(extractFolderPath);
-                directorySize = info.EnumerateFiles().Sum(file => file.Length);
+                // Next line calculate the size (in bytes) of the folder with extracted files (including nested folders)
+                directorySize = Directory.GetFiles(extractFolderPath, "*", SearchOption.AllDirectories).Sum(t => (new FileInfo(t).Length));
+
                 // Size calculation of the original zip file
                 zipFileSize = new FileInfo(zipFilePath).Length;
                 // Compession ratio calculation based on https://en.wikipedia.org/wiki/Data_compression_ratio (see " space savings" equation)
@@ -66,22 +66,26 @@ namespace ZipArchiveApp
             {
                 // "Something went wrong." message will be shown instead of printing the exeption in order to not scary the user
                 Console.WriteLine("Something went wrong. Try again."); 
+                // When extraction will be failed by some reason - user will try again from scratch 
                 UnZip();
             }
+            // Oldest file age calculation code below
             try
             {
-                oldestFile = (new DirectoryInfo(extractFolderPath).GetFiles().OrderByDescending(o => o.LastWriteTime).LastOrDefault()).ToString();
+                // Finding the oldest file in the extracted directory
+                oldestFile = new DirectoryInfo(extractFolderPath).GetFiles().OrderByDescending(o => o.LastWriteTime).LastOrDefault().ToString();
+
             }
             catch
             {
                 Console.WriteLine("Your zip archive did not contain any files. The age of the oldest file can not be calculated");
             }
             
-            string oldestFilePath = Path.Combine(extractFolderPath, oldestFile);
+            string oldestFilePath = Path.Combine(extractFolderPath, oldestFile); // The path to the oldest file
             if (File.Exists(oldestFilePath))
-            {
+            {   // Age of the file calculated in days compared to current system time
                 DateTime creation = File.GetLastWriteTime(oldestFilePath);
-                var currentDateTime = DateTime.Now;
+                DateTime currentDateTime = DateTime.Now;
                 TimeSpan timeElapsed = currentDateTime.Subtract(creation); // Get the TimeSpan of the difference between now and the oldest file modification date
                 double daysAgo = timeElapsed.TotalDays;
                 Console.WriteLine($"The age of the oldest file in the provided zip archive is {daysAgo} days.");
